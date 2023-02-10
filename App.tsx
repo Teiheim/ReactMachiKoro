9b9f3f7a999ce11aba6dd7270f67decf371d07a7
Teiheim <edwards.teiheim1313@gmail.com>
2023-02-10 13:40:24 -0500

    2 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { GameUI } from './GameUI/GameUI';
 import './style.css';
+import { PlayingCard } from './styles';
+
+const CardComponent = (props: any) => {
+  return <PlayingCard></PlayingCard>;
+};
 
 export default function App() {
   return (
     <GameUI>
       <div>
+        <CardComponent></CardComponent>
         <h1>Hello StackBlitz!</h1>
         <p>Start editing to see some magic happen :^)</p>
       </div>
---
+++ styles.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const PlayingCard = styled.div`
+  width:10vw;
+  height:10vh;
+  box-sizing:border-box;
+  border: 5px solid red;
+`;
